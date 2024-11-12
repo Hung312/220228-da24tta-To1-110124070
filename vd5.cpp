c8a8f8d0a607b56f39ecfc705652487353f3a40c
Hung312 <nguyenhungeqqw@gmail.com>
2024-11-12 16:18:59 +0700

    Add files via upload

---
+++ vd5.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	char pheptoan;
+	int a,b;
+	do
+	{
+	printf ("Nhap 2 so nguyen: "); scanf("%d %d",&a,&b);
+	} while (a<b);
+	printf ("Nhap phep toan: "); fflush(stdin); scanf("%c",&pheptoan);
+	switch(pheptoan)
+	{
+	case '+': printf ("Tong la %d",a+b); break;
+	case '-': printf ("Hieu la %d",a-b); break;
+	case '*': printf ("Tich la %d",a*b); break;
+	case '/': printf ("Thuong la: %.2f",(float)a/b); break;
+	}
+	return 0;
+}
