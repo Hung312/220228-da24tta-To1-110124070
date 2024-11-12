90f1f2235175011908d5b98eebd7d753b00ddefd
Hung312 <nguyenhungeqqw@gmail.com>
2024-11-12 15:37:59 +0700

    Add files via upload

---
+++ vd4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int gio;
+	printf ("So gio: "); scanf ("%d",&gio);
+	if (gio<18)
+		printf ("Have a good day");
+	else
+		printf ("Have a good night");
+	return 0;
+}
