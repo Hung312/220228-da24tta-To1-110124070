7d33e4b5ded3da5910292a2ce8feb013e05c33ea
Hung312 <nguyenhungeqqw@gmail.com>
2024-11-19 08:11:51 +0700

    Update baitap13.cpp

--- baitap13.cpp
+++ baitap13.cpp
@@ -1,15 +1,17 @@
-
+#include <stdio.h>
 
 int main()
 {
 	int n;
 	do
 	{
-		printf ("Nhap vao so nguyen n: "); scanf ("%d",&n);
-	}	while (n<=0 || n>=10);
+		printf ("Nhap vao mot so nguyen (0<n<10): "); scanf ("%d",&n);
+	}	while (n>=10||n<=0);
 	int i=1;
-	
-	
-	
+	while (i<=n)
+	{
+		printf ("%d",i);
+		i++;
+	}
 	return 0;
 }
